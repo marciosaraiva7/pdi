3c74a886bbc364ffd686d63c1f2182acef2fc7a9
Márcio Saraiva <73492851+marciosaraiva7@users.noreply.github.com>
2022-12-28 16:28:32 -0300

    remove provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css';
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './themes/defaultTheme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={defaultTheme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 )
