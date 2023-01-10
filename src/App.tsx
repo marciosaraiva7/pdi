300e0b0e7d3de8998e69478d1d87d27c1df849d2
marciosaraiva7 <marciohm7@gmail.com>
2023-01-09 22:29:15 -0300

    provider clear

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import "./App.css";
 import Router from "./pages/routes/routes";
 import { Provider } from "react-redux";
 import { store } from "./store/index";
-import { NextUIProvider, useTheme } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { lightTheme, darkTheme } from "./themes/theme";
 
 function App() {
-  const { isDark, type,theme } = useTheme();
+
   return (
     <Provider store={store}>
       <NextThemesProvider
@@ -18,9 +17,7 @@ function App() {
           dark: darkTheme.className,
         }}
       >
-       
-          <Router />
-          
+        <Router />
       </NextThemesProvider>
     </Provider>
   );
