bae76be21bf5bb49c1ed73e0a10147475ae5a3a1
marciosaraiva7 <marciohm7@gmail.com>
2023-01-18 23:06:47 -0300

    configure default themes

--- src/themes/defaultTheme.ts
+++ src/themes/defaultTheme.ts
@@ -1,44 +1,14 @@
 export const defaultTheme = {
  colors: {
-   brand: {
-     pure: '#2C6EF2',
-     light: '#C5D7FB',
-     medium: '#4985FD',
-     dark: '#0A3A99'
+   light: {
+    background:'#f1f1f1',
+    color: '#000000',
+    subtitle: '#999999'
    },
-   highlight: {
-     pure: '#FF8A00',
-     light: '#FFE3C2',
-     medium: '#CC6E00',
-     dark: '#7A4200'
-   },
-   neutral: {
-     low: {
-       pure: '#000000',
-       light: '#A3A3A3',
-       medium: '#666666',
-       dark: '#292929'
-     },
-     high: {
-       pure: '#FFFFFF',
-       light: '#F5F5F5',
-       medium: '#E0E0E0',
-       dark: '#CCCCCC'
-     }
-   },
-   feedback: {
-     error: {
-       pure: '#EA1F1F',
-       light: '#FDC4C4',
-       medium: '#F05656',
-       dark: '#710A0A'
-     },
-     success: {
-       pure: '#3AA141',
-       light: '#D2EFD4',
-       medium: '#78CE7E',
-       dark: '#205A24'
-     }
+   dark: {
+    background:'#000000',
+    color: '#f1f1f1',
+    subtitle: '#dadedf'
    }
  }
 }
