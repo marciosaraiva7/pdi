d06567e22d23f44791a101226fb7d8ed2b30b228
marciosaraiva7 <marciohm7@gmail.com>
2023-03-29 17:07:42 -0300

    Mark I - conection to GPT

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,32 +1,66 @@
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useDecoder from "../../hooks/useDecoder";
-import { Container, ContainerText, SubTitle, Title } from "./styles";
-import { useSelector } from "react-redux";
-
-import { useEffect, useState } from "react";
-import Player from "../../components/player";
-import { spotifyToken } from "../../store/music/reducer";
+import { Container, ContainerText, SubTitle, Title, Input } from "./styles";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
-  const tokenSpotify = useSelector((state: any) => state.music.spotifyToken);
+  const [message, setMessage] = useState("");
+  const [botResponse, setBotResponse] = useState("");
 
   const decode = useDecoder(token ?? "");
   const name = decode.name;
   const image = decode.image;
+  const requestHeaders: HeadersInit = new Headers();
+  requestHeaders.set("Content-Type", "application/json");
 
   function toProfile() {
     navigate("/profile");
   }
-  console.log(import.meta.env.VITE_Open_AI_Key)
+
+  async function HandleForm(e: any) {
+    e.preventDefault();
+    fetch("http://localhost:3000/api/AI/chatAI", {
+      method: "POST",
+      headers: {
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Content-Type": "application/json",
+        Connection: "keep-alive",
+      },
+      body: JSON.stringify({
+        message,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => setBotResponse(data.message.content.replace("Jarvis: ", "")))
+      .catch((error) => console.log(error));
+  }
+
+  function createMarkup() {
+    return { __html: botResponse };
+  }
 
   return (
     <Container>
       <ContainerText>
         <Title>Olá,{name}</Title>
-        <SubTitle>Pesquise aqui prévia de musicas que você adora</SubTitle>
+        <SubTitle>Pergunte o que quiser</SubTitle>
+        <div>
+          <div dangerouslySetInnerHTML={createMarkup()} />
+        </div>
       </ContainerText>
+      <form onSubmit={(e) => HandleForm(e)}>
+        <Input
+          placeholder="escreva algo"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+      <button type="submit" onClick={HandleForm}>
+        Enviar
+      </button>
     </Container>
   );
 };
--- src/pages/dashboard/styles.ts
+++ src/pages/dashboard/styles.ts
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   padding: 10vh 10px 0 10px;
-  background-color: ${props => props.theme.background}
 `;
 
 export const Title = styled.p`
   margin: 0;
-  color: ${props => props.theme.color};
+  color: ${(props) => props.theme.color};
   text-align: left;
   font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
   font-size: 48px;
@@ -17,12 +20,24 @@ export const Title = styled.p`
 `;
 
 export const SubTitle = styled.p`
-  font-family: 'Inter';
+  font-family: "Inter";
   font-weight: 400;
   color: #999;
+  margin-bottom: 2rem;
 `;
 
 export const ContainerText = styled.div`
-  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  min-width: 35rem;
+  padding: 1rem 1rem 3rem 1rem;
 `;
-
+export const Input = styled.textarea`
+  min-width: 35rem;
+  min-height: 7rem;
+  padding: 1rem;
+  border-radius: 0.8rem;
+  background: #222222;
+  color: #dadedf;
+`;
